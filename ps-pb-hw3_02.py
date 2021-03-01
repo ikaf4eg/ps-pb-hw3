67239256a3e22c28fa72de82b1a98a58f22de6d3
ikaf4eg <ikaf4eg@yandex.ru>
2021-03-02 00:08:26 +0300

    homework3 задание 2

--- ps-pb-hw3_02.py
+++ ps-pb-hw3_02.py
@@ -1,21 +1,34 @@
+'''Залача по выводу суммы чисел из диапазона от min_var до max_var 
+включительно, которые делятся и на 3 и на 5 без остатка.
+'''
+# Функция получения списка чисел из диапазона, 
+# которые делятся на 3 и на 5 без остатка
 def FizzBuzz (min_var,max_var):
     FizzBuzz_list = []
-    FizzBuzz_sum = 0
-    if min_var != None and max_var != None:
-        i = min_var
-        while i >= min_var and i <= max_var:
-            x = i % 3
-            y = i % 5
-            if x == 0 and y == 0:
-                FizzBuzz_list.append(i)
-            i+=1
-    else:
-        FizzBuzz_list = 'не хватает краевых условий'
+    i = min_var
+    while i >= min_var and i <= max_var:
+        x = i % 3
+        y = i % 5
+        if x == 0 and y == 0:
+            FizzBuzz_list.append(i)
+        i+=1
+    return FizzBuzz_list
+
+# Функция вычисления суммы чисел из диапазона, 
+# которые делятся и на 3 и на 5 без остатка
+def FizzBuzz_sum(list):
     f = 0
-    while f < len(FizzBuzz_list):
-        FizzBuzz_sum += FizzBuzz_list[f]
+    FizzBuzz_sum = 0
+    while f < len(list):
+        FizzBuzz_sum += list[f]
         f+=1
     return FizzBuzz_sum
 
 
-print(FizzBuzz(1000,20000))
+# Проверка работы функций
+try: 
+    FB_list = FizzBuzz(1000,20000)
+    print(FizzBuzz_sum(FB_list))
+# В случаен ошибки в функции FizzBuzz ->
+except:
+    print('Ошибка! Не зватает аргументов')
